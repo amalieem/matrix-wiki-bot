1c5f8f8d3b36545a0ba82c7c2cd40101c15bb87a
Amalie Mansåker <amalie.mansaker@gmail.com>
2023-05-17 11:12:06 +0200

    Set up echo-bot

--- index.ts
+++ index.ts
@@ -5,10 +5,40 @@ const AutojoinRoomsMixin = sdk.AutojoinRoomsMixin;
 
 require("dotenv").config() // Load ./.env into environment variables
 
-const homeserverUrl: String = process.env.MX_SERVER_URL || "";
-const accessToken: String = process.env.MX_ACCESS_TOKEN || "";
+
+const COMMAND_PREFIX = "!wiki ";
+const homeserverUrl: string = process.env.MX_SERVER_URL || "";
+const accessToken: string = process.env.MX_ACCESS_TOKEN || "";
+const storage = new SimpleFsStorageProvider("bot.json");
 
 if (!homeserverUrl || !accessToken) {
     console.error("Please supply MX_SERVER_URL and MX_ACCESS_TOKEN in .env");
     process.exit();
+}
+
+
+const client = new MatrixClient(homeserverUrl, accessToken, storage);
+// AutojoinRoomsMixin.setupOnClient(client);
+
+client.start().then(() => console.log("Client started!"));
+client.on("room.message", handleMessage);
+
+async function handleMessage(roomId: string, event: any) {
+    if (event['content']?.['msgtype'] !== 'm.text') return;
+
+    const sender: string = event["sender"];
+    const body: string = event["content"]["body"];
+
+    if (sender === await client.getUserId()) return; // Ignore messages sent from this bot
+    if (!body.startsWith(COMMAND_PREFIX)) return; // Ignore messages not related to us
+
+    console.log(`${roomId}: ${sender} says '${body}`);
+
+    if (body.startsWith(COMMAND_PREFIX + "echo")) {
+        const replyText: string = body.substring("!wiki echo".length).trim();
+        client.sendMessage(roomId, {
+            "msgtype": "m.notice",
+            "body": replyText,
+        });
+    }
 }
